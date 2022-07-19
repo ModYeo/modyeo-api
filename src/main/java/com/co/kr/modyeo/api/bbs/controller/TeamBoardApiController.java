b357708e4c50ee986995843d1f073d00c9642288
anjonggil <qws458@naver.com>
2022-07-19 11:11:48 +0900

    team article 상세 조회

--- src/main/java/com/co/kr/modyeo/api/bbs/controller/TeamBoardApiController.java
+++ src/main/java/com/co/kr/modyeo/api/bbs/controller/TeamBoardApiController.java
@@ -1,9 +1,15 @@
 package com.co.kr.modyeo.api.bbs.controller;
 
+import com.co.kr.modyeo.api.bbs.domain.dto.request.ArticleRequest;
+import com.co.kr.modyeo.api.bbs.domain.dto.request.TeamArticleRequest;
+import com.co.kr.modyeo.api.bbs.domain.dto.response.ArticleDetail;
+import com.co.kr.modyeo.api.bbs.domain.dto.response.TeamArticleDetail;
 import com.co.kr.modyeo.api.bbs.service.TeamBoardService;
+import com.co.kr.modyeo.common.result.JsonResultData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/team-board")
@@ -11,4 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamBoardApiController {
 
     private final TeamBoardService teamBoardService;
+
+    @GetMapping("/article/{article_id}")
+    public ResponseEntity<?> getArticle(
+            @PathVariable(value = "article_id")Long id
+    ){
+        TeamArticleDetail articleDetail = teamBoardService.getTeamArticle(id);
+        return ResponseEntity.ok(JsonResultData.successResultBuilder()
+                .data(articleDetail)
+                .build());
+    }
+
+    @PostMapping("/article")
+    public ResponseEntity<?> createArticle(@RequestBody TeamArticleRequest articleRequest){
+        teamBoardService.createTeamArticle(articleRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(JsonResultData.successResultBuilder()
+                        .data(null)
+                        .build());
+    }
 }
