53070d16149db41515090d333923379402786116
ANJONGGIL <qws458@naver.com>
2022-08-30 07:21:09 +0900

    친구 API
    - 세팅 작업

---
+++ src/main/java/com/co/kr/modyeo/api/member/friend/controller/FriendController.java
@@ -0,0 +1,9 @@
+package com.co.kr.modyeo.api.member.friend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FriendController {
+}
