c98c4965795d953fdd5ede7889a1a17b5fc6c8b3
ANJONGGIL <qws458@naver.com>
2022-08-31 07:27:47 +0900

    친구 API 세팅

---
+++ src/main/java/com/co/kr/modyeo/api/member/friend/service/FriendService.java
@@ -0,0 +1,4 @@
+package com.co.kr.modyeo.api.member.friend.service;
+
+public interface FriendService {
+}
---
+++ src/main/java/com/co/kr/modyeo/api/member/friend/service/impl/FriendServiceImpl.java
@@ -0,0 +1,10 @@
+package com.co.kr.modyeo.api.member.friend.service.impl;
+
+import com.co.kr.modyeo.api.member.friend.service.FriendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FriendServiceImpl implements FriendService {
+}
