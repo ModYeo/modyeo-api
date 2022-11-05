b5c3caff2ecbd16f2f842af87e0b633cc39bc150
skysrd <skysrd@gmail.com>
2022-11-05 20:58:47 +0900

    [FIX] Redis Password Config

--- src/main/java/com/co/kr/modyeo/config/RedisRepositoryConfig.java
+++ src/main/java/com/co/kr/modyeo/config/RedisRepositoryConfig.java
@@ -1,6 +1,7 @@
 package com.co.kr.modyeo.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisRepositoryConfig {
     private final RedisProperties redisProperties;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName(redisProperties.getHost());
+        lettuceConnectionFactory.setPort(redisProperties.getPort());
+        lettuceConnectionFactory.setPassword(redisProperties.getPassword());
+        return lettuceConnectionFactory;
     }
 
     @Bean
