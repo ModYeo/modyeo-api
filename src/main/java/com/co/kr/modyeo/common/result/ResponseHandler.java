ec8e223eeaa091e4005039d5e87b64ad06d20397
anjonggil <qws458@naver.com>
2022-11-15 09:39:06 +0900

    [FIX] Member API
    - ResponseHandler return 변경

--- src/main/java/com/co/kr/modyeo/common/result/ResponseHandler.java
+++ src/main/java/com/co/kr/modyeo/common/result/ResponseHandler.java
@@ -1,5 +1,6 @@
 package com.co.kr.modyeo.common.result;
 
+import com.co.kr.modyeo.common.exception.code.MemberErrorCode;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,16 @@ public class ResponseHandler {
     public static ResponseEntity<JsonResultData<?>> generateResponse(Object data, HttpStatus status){
         JsonResultData<Object> result = JsonResultData.successResultBuilder()
                 .data(data)
+                .build();
+
+        return new ResponseEntity<>(result, status);
+    }
+
+    @Builder(builderClassName = "failResultGenerate",builderMethodName = "failResultGenerate")
+    public static ResponseEntity<JsonResultData<?>> generateResponse(String errorMessage,String errorCode, HttpStatus status){
+        JsonResultData<Object> result = JsonResultData.failResultBuilder()
+                .errorMessage(errorMessage)
+                .errorCode(errorCode)
                 .build();
 
         return new ResponseEntity<>(result, status);
