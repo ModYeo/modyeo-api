7865376b067f9fa41febff2468c754aaefff5174
anjonggil <qws458@naver.com>
2022-12-13 14:09:31 +0900

    [FIX] SearchDto 추가
    - search parameter 변경

--- src/main/java/com/co/kr/modyeo/api/bbs/domain/dto/search/TeamArticleSearch.java
+++ src/main/java/com/co/kr/modyeo/api/bbs/domain/dto/search/TeamArticleSearch.java
@@ -1,14 +1,13 @@
 package com.co.kr.modyeo.api.bbs.domain.dto.search;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.co.kr.modyeo.common.dto.SearchDto;
+import lombok.*;
 import org.springframework.data.domain.Sort;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TeamArticleSearch {
+public class TeamArticleSearch extends SearchDto {
     private String title;
 
     private Long teamId;
@@ -17,23 +16,11 @@ public class TeamArticleSearch {
 
     private Long memberId;
 
-    private Integer limit;
-
-    private Integer offset;
-
-    private String orderBy;
-
-    private Sort.Direction direction;
-
     @Builder(builderMethodName = "of", builderClassName = "of")
-    public TeamArticleSearch(Long teamId, String title, String content, Integer limit, Integer offset, String orderBy, Sort.Direction direction, Long memberId) {
+    public TeamArticleSearch(Long teamId, String title, String content, Long memberId) {
         this.title = title;
         this.teamId = teamId;
         this.content = content;
         this.memberId = memberId;
-        this.limit = limit != null ? limit : 20;
-        this.offset = offset != null ? offset : 0;
-        this.orderBy = orderBy != null ? orderBy : "id";
-        this.direction = direction != null ? direction : Sort.Direction.DESC;
     }
 }
