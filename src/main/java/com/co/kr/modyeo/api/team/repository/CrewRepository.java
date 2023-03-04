f647b544fc9d5e82f2f7ed02c4ff17feee158bf9
ANJONGGIL <qws458@naver.com>
2023-03-04 13:56:57 +0900

    [Fix] Crew API
    - 가입 신청 제약 추가

--- src/main/java/com/co/kr/modyeo/api/team/repository/CrewRepository.java
+++ src/main/java/com/co/kr/modyeo/api/team/repository/CrewRepository.java
@@ -1,5 +1,7 @@
 package com.co.kr.modyeo.api.team.repository;
 
+import com.co.kr.modyeo.api.member.domain.entity.Member;
+import com.co.kr.modyeo.api.team.domain.entity.Team;
 import com.co.kr.modyeo.api.team.domain.entity.enumerate.CrewLevel;
 import com.co.kr.modyeo.api.team.domain.entity.link.Crew;
 import com.co.kr.modyeo.api.team.repository.custom.CrewCustomRepository;
@@ -7,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewRepository extends JpaRepository<Crew,Long>, CrewCustomRepository {
     CrewLevel findCrewLevelById(Long id);
+
+    Crew findByTeamAndMember(Team team, Member member);
 }
