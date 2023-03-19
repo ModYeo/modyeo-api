b3495fdc99bfec4ca4b76c3fd18ec4abd77fcdba
Giri an <42487599+Anjonggil@users.noreply.github.com>
2023-03-19 19:48:52 +0900

    [MOD-233] TEAM API (#104)
    - 팀 가입 신청 삭제시 Transactional 누락으로 인한 동작 X

--- src/test/java/com/co/kr/modyeo/member/service/impl/TeamApplicationServiceImplTest.java
+++ src/test/java/com/co/kr/modyeo/member/service/impl/TeamApplicationServiceImplTest.java
@@ -14,6 +14,9 @@ import com.co.kr.modyeo.api.member.repository.MemberRepository;
 import com.co.kr.modyeo.api.team.service.TeamApplicationService;
 import com.co.kr.modyeo.api.team.service.impl.TeamApplicationServiceImpl;
 import com.co.kr.modyeo.common.enumerate.Yn;
+import com.co.kr.modyeo.common.exception.ApiException;
+import com.co.kr.modyeo.common.exception.code.BoardErrorCode;
+import com.co.kr.modyeo.common.exception.code.TeamErrorCode;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,10 +25,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -49,6 +54,21 @@ class TeamApplicationServiceImplTest {
 
     @Mock
     private ApplicationFormRepository applicationFormRepository;
+
+    Member FIXTURE_MEM_01 = Member.of()
+            .id(1L)
+            .email("qws458@naver.com")
+            .build();
+
+    Team FIXTURE_TEAM_01 = Team.of()
+            .id(1L)
+            .name("test crew")
+            .build();
+
+    MemberTeam FIXTURE_MEM_TEAM_01 = MemberTeam.joinApplicationBuilder()
+            .team(FIXTURE_TEAM_01)
+            .member(FIXTURE_MEM_01)
+            .build();
 
     @BeforeEach
     public void setup() {
@@ -118,5 +138,29 @@ class TeamApplicationServiceImplTest {
         then(applicationFormRepository).should().findById(any());
 
         assertThat(applicationForm.getContent()).isEqualTo(applicationFormRequest.getContent());
+    }
+
+    @Test
+    void deleteTeamApplication(){
+        given(memberTeamRepository.findById(any())).willReturn(Optional.of(FIXTURE_MEM_TEAM_01));
+
+        teamApplicationService.deleteTeamApplication(FIXTURE_MEM_TEAM_01.getId());
+
+        then(memberTeamRepository).should().findById(any());
+        then(memberTeamRepository).should().delete(any());
+    }
+
+    @Test
+    void deleteTeamApplicationFail(){
+        given(memberTeamRepository.findById(any())).willThrow(ApiException.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .errorMessage(TeamErrorCode.NOT_FOUND_APPLICANT.getMessage())
+                .errorCode(TeamErrorCode.NOT_FOUND_APPLICANT.getCode())
+                .build());
+
+        assertThatThrownBy(() -> {
+            teamApplicationService.deleteTeamApplication(any());
+        }).isInstanceOf(Exception.class)
+                .hasMessageContaining(TeamErrorCode.NOT_FOUND_APPLICANT.getMessage());
     }
 }
