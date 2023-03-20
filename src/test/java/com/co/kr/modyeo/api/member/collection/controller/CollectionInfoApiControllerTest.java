92aa66897e9bdcb56049a1e2c0de94e06cbdd593
ANJONGGIL <qws458@naver.com>
2023-03-20 15:40:16 +0900

    [MOD-235] CollectionInfo API
    - 데이터 삭제 시 MemberCollectionInfo가 존재 할 시 삭제되지 않았던 이슈 발생

---
+++ src/test/java/com/co/kr/modyeo/api/member/collection/controller/CollectionInfoApiControllerTest.java
@@ -0,0 +1,38 @@
+package com.co.kr.modyeo.api.member.collection.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+@AutoConfigureMockMvc
+class CollectionInfoApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser
+    @Transactional
+    void deleteCollectionInfo() throws Exception {
+        mockMvc.perform(delete("/api/collection-info/56"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+}
